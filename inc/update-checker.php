3c630ce539b33abe66f6c8c33a46dd427035ad3d
Tales Henrique <tales.henrique@ollyolly.com>
2026-01-08 19:59:05 -0300

    feat: configurar Plugin Update Checker com GitHub releases

--- inc/update-checker.php
+++ inc/update-checker.php
@@ -1,13 +1,13 @@
 <?php
 
-use YahnisElsts\PluginUpdateChecker\v5p4\Theme\UpdateChecker;
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
-// Initialize the theme update checker with the correct class for themes
-$updateChecker = new UpdateChecker(
-    'https://github.com/tales-bluecrocus/byrde',
-    __FILE__,
+// Initialize the update checker for GitHub
+$myUpdateChecker = PucFactory::buildUpdateChecker(
+    'https://github.com/tales-bluecrocus/byrde/',
+    get_template_directory() . '/functions.php',
     'byrde',
 );
 
 // Enable release assets - uses GitHub Releases instead of branch ZIP
-$updateChecker->getVcsApi()->enableReleaseAssets();
+$myUpdateChecker->getVcsApi()->enableReleaseAssets();
