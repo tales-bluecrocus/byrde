86d9346017a09c3b34f1a3689405b6fca6e6da81
Tales Henrique <tales.henrique@ollyolly.com>
2026-01-08 19:38:18 -0300

    fix: detectar slug do tema automaticamente

--- inc/update-checker.php
+++ inc/update-checker.php
@@ -2,11 +2,14 @@
 
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
+// Get the theme slug (folder name)
+$theme_slug = basename(get_template_directory());
+
 // Initialize the theme update checker
 $updateChecker = PucFactory::buildUpdateChecker(
     'https://github.com/tales-bluecrocus/byrde',
-    __DIR__ . '/../style.css',
-    'byrde',
+    get_stylesheet_directory() . '/style.css',
+    $theme_slug,
 );
 
 // Enable release assets - uses GitHub Releases instead of branch ZIP
