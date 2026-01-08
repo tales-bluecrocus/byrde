00afee11cf71b3b630e187e7b39aa1a8928deb5f
Tales Henrique <tales.henrique@ollyolly.com>
2026-01-08 13:31:35 -0300

    start theme setup

---
+++ .config/stylelint.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['stylelint-config-standard-scss', 'stylelint-config-prettier'],
+  rules: {
+    indentation: 'tab',
+    'at-rule-no-unknown': null,
+  },
+};
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+echo 'Hello, World!';
+echo 'This is a sample PHP file.';
+
+function greet($name)
+{
+    return 'Hello, ' . $name . '!';
+}
