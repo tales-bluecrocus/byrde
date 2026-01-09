86e467c8bdb518faf3bbbd8ebf897e06ccbc2846
Tales Henrique <tales.henrique@ollyolly.com>
2026-01-09 13:54:33 -0300

    update required itens

--- inc/admin/required-plugins.php
+++ inc/admin/required-plugins.php
@@ -1,9 +1,10 @@
 <?php
+
 /**
  * Required Plugins Configuration
- * 
+ *
  * This file uses TGM Plugin Activation to manage required and recommended plugins
- * 
+ *
  * @package Byrde
  */
 
@@ -18,6 +19,7 @@ $tgm_path = get_template_directory() . '/vendor/tgmpa/tgm-plugin-activation/clas
 if (!file_exists($tgm_path)) {
     // Log error and return silently - plugin management will not be available
     error_log('Byrde Theme: TGM Plugin Activation not found at ' . $tgm_path);
+
     return;
 }
 
@@ -34,31 +36,31 @@ function byrde_register_required_plugins()
      * Array of plugin arrays. Required keys are name and slug.
      * If the source is NOT from the .org repo, then source is also required.
      */
-    $plugins = array(
+    $plugins = [
         // Site Kit by Google - Required plugin for Google Analytics and Search Console integration
-        array(
+        [
             'name'     => 'Site Kit by Google',
             'slug'     => 'google-site-kit',
             'required' => true,
-        ),
+        ],
         // WP Activity Log - Required plugin
-        array(
+        [
             'name'     => 'WP Activity Log',
             'slug'     => 'wp-security-audit-log',
             'required' => true,
-        ),
+        ],
         // Query Monitor - Required plugin
-        array(
+        [
             'name'     => 'Query Monitor',
             'slug'     => 'query-monitor',
             'required' => true,
-        ),
-    );
+        ],
+    ];
 
     /**
      * Array of configuration settings
      */
-    $config = array(
+    $config = [
         'id'           => 'byrde',                  // Unique ID for hashing notices for multiple instances of TGMPA
         'default_path' => '',                       // Default absolute path to bundled plugins
         'menu'         => 'tgmpa-install-plugins',  // Menu slug
@@ -69,7 +71,7 @@ function byrde_register_required_plugins()
         'dismiss_msg'  => '',                       // If 'dismissable' is false, this message will be output at top of nag
         'is_automatic' => true,                     // Automatically activate plugins after installation or not
         'message'      => '<p><strong>This theme requires the following plugin to function properly. Please install and activate it.</strong></p>',
-        'strings'      => array(
+        'strings'      => [
             'page_title'                      => __('Install Required Plugins', 'byrde'),
             'menu_title'                      => __('Install Plugins', 'byrde'),
             'installing'                      => __('Installing Plugin: %s', 'byrde'),
@@ -78,47 +80,47 @@ function byrde_register_required_plugins()
             'notice_can_install_required'     => _n_noop(
                 'This theme requires the following plugin: %1$s.',
                 'This theme requires the following plugins: %1$s.',
-                'byrde'
+                'byrde',
             ),
             'notice_can_install_recommended'  => _n_noop(
                 'This theme recommends the following plugin: %1$s.',
                 'This theme recommends the following plugins: %1$s.',
-                'byrde'
+                'byrde',
             ),
             'notice_ask_to_update'            => _n_noop(
                 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.',
                 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.',
-                'byrde'
+                'byrde',
             ),
             'notice_ask_to_update_maybe'      => _n_noop(
                 'There is an update available for: %1$s.',
                 'There are updates available for the following plugins: %1$s.',
-                'byrde'
+                'byrde',
             ),
             'notice_can_activate_required'    => _n_noop(
                 'The following required plugin is currently inactive: %1$s.',
                 'The following required plugins are currently inactive: %1$s.',
-                'byrde'
+                'byrde',
             ),
             'notice_can_activate_recommended' => _n_noop(
                 'The following recommended plugin is currently inactive: %1$s.',
                 'The following recommended plugins are currently inactive: %1$s.',
-                'byrde'
+                'byrde',
             ),
             'install_link'                    => _n_noop(
                 'Begin installing plugin',
                 'Begin installing plugins',
-                'byrde'
+                'byrde',
             ),
             'update_link'                     => _n_noop(
                 'Begin updating plugin',
                 'Begin updating plugins',
-                'byrde'
+                'byrde',
             ),
             'activate_link'                   => _n_noop(
                 'Begin activating plugin',
                 'Begin activating plugins',
-                'byrde'
+                'byrde',
             ),
             'return'                          => __('Return to Required Plugins Installer', 'byrde'),
             'plugin_activated'                => __('Plugin activated successfully.', 'byrde'),
@@ -130,8 +132,8 @@ function byrde_register_required_plugins()
             'notice_cannot_install_activate'  => __('There are one or more required or recommended plugins to install, update or activate.', 'byrde'),
             'contact_admin'                   => __('Please contact the administrator of this site for help.', 'byrde'),
             'nag_type'                        => 'updated',
-        ),
-    );
+        ],
+    ];
 
     tgmpa($plugins, $config);
 }
