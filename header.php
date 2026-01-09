4ce54d8b5acb0a585f7c82d9ffdac99e18f073f6
Tales Henrique <tales.henrique@ollyolly.com>
2026-01-09 01:08:58 -0300

    feat: test header update

--- header.php
+++ header.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?php bloginfo('name'); ?> | <?php bloginfo('description'); ?></title>
     <?php wp_head(); ?>
 </head>
 <body>
 
 
 <header class="header container">
-    <h1><?php bloginfo('name'); ?></h1>
+    <h1><?php bloginfo('name'); ?> | <?php bloginfo('description'); ?></h1>
     <h2>Welcome to the Header</h2>
 </header>
